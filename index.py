d4760bca20715278fea84f0e612f636c66a4da43
Anik Sikder <aniksikder9501@gmail.com>
2023-06-28 02:53:33 +0600

    Grade Point Calculation

---
+++ index.py
@@ -0,0 +1,19 @@
+bangla = int(input('Please enter your marks for Bangla? '))
+english = int(input('Please enter your marks for English? '))
+math = int(input('Please enter your marks for Math? '))
+science = int(input('Please enter your marks for Science? '))
+
+avr= (bangla + english + math + science) / 4
+
+if avr < 40:
+    print('You are fail')
+elif avr > 40:
+    print('Your grade point is D')
+elif avr > 60:
+    print('Your grade point is C')
+elif avr > 70:
+    print('Your grade point is B')
+elif avr > 80:
+    print('Your grade point is A')
+else:
+    print('Your grade point is A+')
